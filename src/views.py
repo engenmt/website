ee4d5424f884a0318c275fccd07c48401dd55454
Michael Engen <engenmt@gmail.com>
2022-05-07 20:47:49 -0400

    Functional navbar complete

--- src/views.py
+++ src/views.py
@@ -5,11 +5,27 @@ bp = Blueprint("views", __name__)
 
 
 @bp.route("/")
+@bp.route("/home")
 def home() -> str:
-    return render_template("hello.html")
+    return render_template("base.html")
+
+
+@bp.route("/research")
+def research() -> str:
+    my_articles = get_articles()
+    return render_template("articles.html", articles=my_articles)
+
+
+@bp.route("/software")
+def software() -> str:
+    return render_template("base.html")
+
+
+@bp.route("/about")
+def about() -> str:
+    return render_template("base.html")
 
 
 @bp.route("/articles")
 def articles() -> str:
-    my_articles = get_articles()
-    return render_template("articles.html", articles=my_articles)
+    return render_template("base.html")

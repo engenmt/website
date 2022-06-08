4bef066d9fe9bacafcd5d30dd3d63a0a6e39c600
Michael Engen <engenmt@gmail.com>
2022-06-08 06:45:07 -0400

    Added types

--- src/articles.py
+++ src/articles.py
@@ -1,8 +1,7 @@
 from flask import current_app
 import yaml
 
-
-LOREM_IPSUM = (
+LOREM_IPSUM: str = (
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
     "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut "
     "enim ad minim veniam, quis nostrud exercitation ullamco laboris "
@@ -13,7 +12,7 @@ LOREM_IPSUM = (
 )
 
 
-def create_authors_string(authors):
+def create_authors_string(authors: list[str]) -> str:
     if len(authors) >= 2:
         authors[-1] = f"and {authors[-1]}"
     if len(authors) >= 3:
@@ -22,8 +21,8 @@ def create_authors_string(authors):
     return " ".join(authors)
 
 
-def create_links(article):
-    links = []
+def create_links(article: dict) -> list[tuple[str, str]]:
+    links: list[tuple[str, str]] = []
     if "arxiv" in article:
         links.append(("arXiv", article["arxiv"]))
     if "journal" in article:
@@ -33,14 +32,15 @@ def create_links(article):
     return links
 
 
-def choose_image(article):
-    images = article["images"]
-    return images.get("small", images.get("medium", images.get("large")))
+def choose_image(article: dict) -> str:
+    images: dict = article["images"]
+    image: str = images.get("small", images.get("medium", images.get("large")))
+    return f"content/{image}"
 
 
-def get_articles():
+def get_articles() -> list[dict]:
     with current_app.open_resource("static/content/articles.yaml") as f:
-        my_articles = yaml.load(f, Loader=yaml.Loader)
+        my_articles: list[dict] = yaml.load(f, Loader=yaml.Loader)
 
     for article in my_articles:
         article["authors_string"] = create_authors_string(article["authors"])
@@ -49,8 +49,6 @@ def get_articles():
         if "abstract" not in article:
             article["abstract"] = LOREM_IPSUM
 
-        image = choose_image(article)
-
-        article["image"] = f"content/{image}"
+        article["image"] = choose_image(article)
 
     return my_articles

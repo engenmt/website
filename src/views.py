e0d8ec1a8f1da974d97b00499989ae08d4d3097d
Michael Engen <engenmt@gmail.com>
2022-07-07 17:19:00 -0400

    Removed view added too early

--- src/views.py
+++ src/views.py
@@ -37,8 +37,3 @@ def articles() -> str:
 @bp.route("/permpy")
 def perpy_view() -> str:
     return render_template("permpy-repl.html")
-
-
-@bp.route("/enumerate")
-def perpy_enumerate() -> str:
-    return render_template("permpy-enumerate.html")

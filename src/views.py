e2ef379a03fc4022ae9d6c0ee1617b05d433882b
Michael Engen <engenmt@gmail.com>
2022-07-07 17:16:30 -0400

    It *just* works.

--- src/views.py
+++ src/views.py
@@ -36,4 +36,9 @@ def articles() -> str:
 
 @bp.route("/permpy")
 def perpy_view() -> str:
-    return render_template("permpy.html")
+    return render_template("permpy-repl.html")
+
+
+@bp.route("/enumerate")
+def perpy_enumerate() -> str:
+    return render_template("permpy-enumerate.html")
